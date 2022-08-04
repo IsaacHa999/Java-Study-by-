5c3790b586a7de45d4a3dad8271c9af771866674
Isaac Ha <gladisaa5441@gmail.com>
2022-08-04 23:11:07 +0900

    five commit

--- src/Sample.java
+++ src/Sample.java
@@ -2,7 +2,7 @@ public class Sample {
     public static void main(String[] args) {
         int a = 80;
         int b = 75;
-        int c = 55;
+        int c = 55123;
         System.out.println((a + b + c) / 3); // 70 출력
     }
 }
