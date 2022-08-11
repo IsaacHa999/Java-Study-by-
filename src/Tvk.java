4ca18a48527f52c0b2779ee105f9725a50598692
Isaac Ha <gladisaa5441@gmail.com>
2022-08-11 21:09:43 +0900

    six commit

--- src/Tvk.java
+++ src/Tvk.java
@@ -7,6 +7,9 @@ class Tv {
     String color;
     boolean power;
     int channel;
+    String color;
+    String volume;
+
 
     void power()	{ power = !power; }
     void channelUp()	{ ++channel; }
